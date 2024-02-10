ec7e851ca1cd731d773acbaaa15bb8d43d8bbaad
Phil Broadley <philbro1992@hotmail.co.uk>
2024-02-10 15:10:14 +0000

    new routes

--- sever.js
+++ sever.js
@@ -1,3 +1,4 @@
+//Packages
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -5,8 +6,13 @@ require("dotenv").config();
 const bodyParser = require("body-parser");
 const { Pool } = require("pg");
 const { status } = require("init");
+const Postal = require("@atech/postal");
+let postalClient = new Postal.Client(
+    "https://postal.yourdomain.com",
+    "your-api-key"
+);
 
-//DB Setup
+//PG DB Setup
 const pool = new Pool({
     user: "postgres",
     host: "localhost",
@@ -22,8 +28,35 @@ app.use(bodyParser.json());
 
 //Routes
 
+//Login staff member
+app.post(`/login`, async (req, res) => {
+    try {
+        const body = req.body;
+        const client = await pool.connect();
+        let checkExistsSQL = `SELECT *
+        FROM staff WHERE email = '${body.email}'`;
+
+        await client
+            .query(checkExistsSQL)
+            .then(async (resp) => {
+                if (resp.rows.length > 0) {
+                    if (!bycyprtresp.rows[0].password) {
+                    }
+                } else {
+                    res.status(204).json({ message: "User does not exist" });
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    } catch (err) {
+        console.error("Error executing query", err);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
 //Get all customers
-app.get("/all-customers", async (req, res, next) => {
+app.get("/all-customers", async (req, res) => {
     try {
         const client = await pool.connect();
         const sql =
@@ -88,8 +121,8 @@ app.post(`/customer-rentals`, async (req, res) => {
         client.release();
         res.status(200).json(rentals);
     } catch (err) {
-        console.log(err);
-        res.status(500).json(err);
+        console.error("Error executing query", err);
+        res.status(500).json({ error: "Internal Server Error" });
     }
 });
 
@@ -106,8 +139,8 @@ app.get(`/all-cities`, async (req, res) => {
         client.release();
         res.status(200).json(cities);
     } catch (err) {
-        console.log(err);
-        res.status(500).json(err);
+        console.error("Error executing query", err);
+        res.status(500).json({ error: "Internal Server Error" });
     }
 });
 
@@ -120,31 +153,130 @@ app.post(`/new-customer`, async (req, res) => {
             address, address2, city_id, postal_code, phone )
             VALUES ( '${customer.address}', '${customer.address2}', ${customer.city}, '${customer.postal_code}', '${customer.phone}' ) returning address_id`;
 
+        let checkExistsSQL = `SELECT email
+        FROM customer WHERE email = '${req.body.email}'`;
+
         await client
-            .query(addressSql)
+            .query(checkExistsSQL)
             .then(async (resp) => {
-                console.log(resp.rows.address_id);
-                const address_id = resp.rows[0].address_id;
-                let customerSql = `INSERT INTO public.customer(
-                    store_id, first_name, last_name, email, address_id)
-                    VALUES ( ${customer.store_id}, '${customer.first_name}', '${customer.last_name}', '${customer.email}', ${address_id})`;
-                console.log(customerSql);
-                await client
-                    .query(customerSql)
-                    .then((resp) => {
-                        console.log("Customer created");
-                    })
-                    .catch((err) => console.log(err));
+                if (resp.rows.length == 0) {
+                    await client
+                        .query(addressSql)
+                        .then(async (resp) => {
+                            console.log(resp.rows.address_id);
+                            const address_id = resp.rows[0].address_id;
+                            let customerSql = `INSERT INTO public.customer(
+                                store_id, first_name, last_name, email, address_id)
+                                VALUES ( ${customer.store_id}, '${customer.first_name}', '${customer.last_name}', '${customer.email}', ${address_id})`;
+                            console.log(customerSql);
+                            await client
+                                .query(customerSql)
+                                .then((resp) => {
+                                    console.log("Customer created");
+                                })
+                                .catch((err) => console.log(err));
+                        })
+                        .catch((err) => console.log(err));
+                } else {
+                    res.status(409).json({
+                        message: "Customer email already exists",
+                    });
+                }
             })
-            .catch((err) => console.log(err));
+            .catch((err) => {
+                console.log(err);
+            });
 
-        console.log(req.body);
-        console.log(addressSql);
-
-        res.status(201);
+        res.status(201).json({ message: "Customer added" });
     } catch (err) {
-        console.log(err);
-        res.status(500);
+        console.error("Error executing query", err);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
+//Get all movies
+app.get(`/all-movies`, async (req, res) => {
+    try {
+        let movies = [];
+        const client = await pool.connect();
+        const sql = `SELECT film.film_id AS film_id, title, description, release_year, rental_duration, rental_rate, length, replacement_cost, rating, special_features, language.name AS language, category.name as category           FROM film 
+            INNER JOIN language 
+            ON film.language_id = language.language_id
+            INNER JOIN film_category 
+            ON film.film_id = film_category.film_id 
+            INNER  JOIN category
+            ON film_category.category_id = category.category_id`;
+        await client
+            .query(sql)
+            .then(async (resp) => {
+                for (let m = 0; m < resp.rows.length; m++) {
+                    let movie = resp.rows[m];
+
+                    let actorsSql = `SELECT actor.actor_id, first_name, last_name 
+                    FROM film
+                    INNER JOIN film_actor
+                    ON film.film_id = film_actor.film_id
+                    INNER JOIN actor
+                    ON film_actor.actor_id = actor.actor_id WHERE film.film_id = ${resp.rows[m].film_id}`;
+                    const actors = await client
+                        .query(actorsSql)
+                        .then((actorResp) => {
+                            movie.actors = actorResp.rows;
+                            movies.push(movie);
+                        })
+                        .catch((err) => {
+                            console.error("Error executing query", err);
+                            res.status(500).json({
+                                error: "Internal Server Error",
+                            });
+                        });
+                }
+            })
+            .catch((err) => {
+                console.error("Error executing query", err);
+                res.status(500).json({ error: "Internal Server Error" });
+            });
+
+        client.release();
+        res.status(200).json(movies);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
+//Search movie by name
+
+//Get Movie Inventory
+app.post(`/movie-inventory`, async (req, res) => {
+    try {
+        const client = await pool.connect();
+        let inventory = 0;
+        const { filmId } = req.body;
+        const sql = `SELECT film_id, COALESCE(COUNT(*), 0) AS inventory
+        FROM inventory
+        WHERE film_id = ${filmId}
+        GROUP BY film_id`;
+
+        await client
+            .query(sql)
+            .then((resp) => {
+                if (resp.rows) {
+                    inventory = resp.rows[0].inventory;
+                }
+            })
+            .catch((err) => {
+                console.error("Error executing query", err);
+                res.status(500).json({
+                    error: "Internal Server Error",
+                });
+                inventory = 0;
+            });
+        client.release();
+        res.status(200).json({ inventory: inventory });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "Internal Server Error" });
     }
 });
 
